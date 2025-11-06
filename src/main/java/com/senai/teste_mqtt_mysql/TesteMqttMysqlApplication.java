67a3f8a1740f3c201d27d1ff91b132c4b4389a6a
Alex Soares da Silva <alequis19soares@gmail.com>
2025-11-06 10:53:47 -0300

    First commit

---
+++ src/main/java/com/senai/teste_mqtt_mysql/TesteMqttMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.senai.teste_mqtt_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMqttMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteMqttMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/teste_mqtt_mysql/TesteMqttMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.teste_mqtt_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteMqttMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
